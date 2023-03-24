8d569ca47dc55bbb9aa74a2d32783e076967b120
Thiago Rodrigues <thiago.rds@outlook.com.br>
2023-03-24 13:28:00 -0400

    feat: patch client route, controller and service

--- src/controllers/client/client.controller.ts
+++ src/controllers/client/client.controller.ts
@@ -1,13 +1,16 @@
 import { Request, Response } from "express"
-import { createdClientService, deleteClientService, retriveClientService } from "../../services/client/client.service"
+import { createdClientService, deleteClientService, retriveClientService, updateClientService } from "../../services/client/client.service"
 
 const createdClientController = async (req: Request, resp: Response) => { 
      const data = req.body
      const client = await createdClientService(data)
      return resp.status(201).json(client)
 }
-const updateClientController = (req: Request, resp: Response) => { 
-     return resp.status(201).json("chegou no controler")
+const updateClientController = async (req: Request, resp: Response) => {
+     const client = req.user
+     const dataUpdate = req.body
+     const clientUpdate = await updateClientService(client, dataUpdate)
+     return resp.status(201).json(clientUpdate)
 }
 const deleteClientController = async (req: Request, resp: Response) => { 
      const id = req.user.id
--- src/middlewares/availabilityChecker.middleware.ts
+++ src/middlewares/availabilityChecker.middleware.ts
@@ -4,18 +4,24 @@ import { Client } from "../entities/client.entity";
 
 
 const availabilityCheckerMiddleware = async (req: Request, resp: Response, next: NextFunction) => {
-     
      const clientRepository = AppDataSource.getRepository(Client)
+     const email = req.body.email;
+     const nickname = req.body.nickname;
 
-     const emailExist = await clientRepository.findOne({where: { email: req.body.email }})
-     const nicknameExist = await clientRepository.findOne({where: { nickname: req.body.nickname }})
-
-     if (emailExist) { 
-          return resp.status(409).json({message:"E-mail already registered"})
+     if (!email && nickname) {
+          const nicknameExist = await clientRepository.findOne({where: { nickname: nickname }})
+          
+          if (nicknameExist) { 
+               return resp.status(409).json({message:"Nickname already registered"})
+          }
      }
 
-     if (nicknameExist) { 
-          return resp.status(409).json({message:"Nickname already registered"})
+     if (!nickname && email) {
+          const emailExist = await clientRepository.findOne({where: { email: email }})
+          
+          if (emailExist) { 
+               return resp.status(409).json({message:"E-mail already registered"})
+          }    
      }
 
      return next()
